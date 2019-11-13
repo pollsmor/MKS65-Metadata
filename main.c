20b10f7a89e89cba1f4a2db140a8130fdc0dc8ab
Kevin Li <kevinli4321@gmail.com>
2019-11-13 17:35:01 -0500

    Use sprintf

--- main.c
+++ main.c
@@ -6,13 +6,16 @@ int main() {
   printf("Printing out metadata of main.c: \n");
   struct stat metadata;
   stat("main.c", &metadata);
+
   printf("File size: \n");
+  char buffer[50];
   printf("> %ld B \n", metadata.st_size);
-  printf("> %lf KB \n", metadata.st_size / 1000.0);
-  printf("> %lf MB \n", metadata.st_size / 1000000.0);
-  if (metadata.st_size > 1000000) //larger than 1MB
-    printf("> %lf GB \n", metadata.st_size / 1000000000.0);
-  printf("\n");
+  sprintf(buffer, "> %f KB \n", metadata.st_size / 1000.0);
+  if (metadata.st_size > 1000000) //larger than 1 MB
+    sprintf(buffer, "> %f MB \n", metadata.st_size / 1000000.0);
+  if (metadata.st_size > 1000000000) //larger than 1 GB
+    sprintf(buffer, "> %f GB \n", metadata.st_size / 1000000000.0);
+  printf("%s \n", buffer);
 
   printf("Last accessed: \n");
   printf("> %s \n", ctime(&metadata.st_atime));
